e0d123b22aaa2f3e39478151dcf8a1950409da0f
abdallahbilal <abdallahbilal39@gmail.com>
2024-06-18 13:59:37 -0400

    added data binding and button references

--- app/src/main/java/ca/georgiancollege/midtermexam/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/midtermexam/MainActivity.kt
@@ -5,16 +5,52 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ca.georgiancollege.midtermexam.databinding.ActivityMainBinding
+
+/*
+*
+* Abdallah Bilal
+* 200494848
+* Tuesday June 18th 2024
+* Comp 3025 Midterm Exam
+*
+* */
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        createButtonReferences()
+}
+
+    private fun createButtonReferences(): Unit {
+        val twoButtons = arrayOf(
+            binding.SumbitButton, binding.CancelButton
+        )
+
     }
+
+
+
+
+
+
+
+
+
 }
